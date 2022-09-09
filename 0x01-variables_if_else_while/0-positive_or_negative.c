abee7e7d06ef3a056efd9edd5981ef00134dac75
LendoHub <lendodlic2022@gmail.com>
2022-09-09 21:50:37 +0000

    Positive, Negative Or Zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,13 +16,13 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0) {	
-		printf("%n is positive\n", n);
+		printf("%d is positive\n", n);
 	}	
 	else if (n < 0) {
-	       	printf("%n is negative\n", n);
+	       	printf("%d is negative\n", n);
 	}
 	else {
-		printf("%i is positive\n", n);
+		printf("%d is positive\n", n);
 	}
 
 	return (0);
