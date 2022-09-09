1cf7b184cf58086406ceca241606e8f4ac1acd98
LendoHub <lendodlic2022@gmail.com>
2022-09-09 23:55:42 +0000

    Last Digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,21 +10,24 @@
 int main(void)
 {
 	int n;
-	char last[] = "Last digit of";
+
 	srand(time(0));
 	n = rand() - RAND_MAX 	/ 2;
 	printf("%s %d is %d and is ", last, n, n % 10);
 	if (n % 10 > 5)
 	{
-	printf("greater than 5/n");
+		printf("Last digit of %i is %i and is greater than 5\n", n, n % 10);
 	}
+
 	else if (n % 10 == 0)
 	{
-	printf("O\n");
+		printf("Last digit of %i is %i and is 0\n", n, n % 10);
 	}
+
 	else
 	{
-	printf("less than 6 and not O\n");
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, n % 10);
 	}
+
 	return (0);
 }
