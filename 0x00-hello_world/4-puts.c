62f47a7539564a3a80890fb77d2c20b62e83a537
LendoHub <lendodlic2022@gmail.com>
2022-09-09 01:45:37 +0000

    Using the puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
 /**
- *main - Printing
- * 
- *Return: Always 0 (Successful)
+ *main - entry point
  *
+ *Return: 0 after printing the function
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"programming is like building a multilingual puzzle");
 	return (0);
 }
