c65d986e36091563fd7f0331e9994b3235dddd38
LendoHub <lendodlic2022@gmail.com>
2022-09-10 02:09:46 +0000

    Combination of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -18,19 +18,20 @@ int main(void)
 		{
 			for (m = 2; m <= 9; m++)
 			{
-			if (m > p && p > i)
-			{
-				putchar(i + '0');
-				putchar(p + '0');
-				putchar(m + '0');
-				if (i != 7)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}	
+				if (m > p && p > i)
+				{	
+					putchar(i + '0');
+					putchar(p + '0');
+					putchar(m + '0');
+					if (i != 7)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}	
+			}
 		}
-	}
+	}	
 	putchar('\n');
 	return (0);
 }
