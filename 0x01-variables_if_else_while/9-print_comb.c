3bfd4397c9baa7219c78a8210987be65f0ca6f5e
LendoHub <lendodlic2022@gmail.com>
2022-09-10 01:14:33 +0000

    Combination of sigle digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+*main - Prints combination of single digit numbers
+*
+*Return: Always (Success);
+*/
+int main(void)
+{
+	int chap;
+
+	for (chap = '0'; chap <= '9'; chap++)
+	{
+		putchar(chap);
+
+		if (chap != '9')
+		{
+			putchar(',');																				putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
