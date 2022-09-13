8b157f2a03dc786cc0ae197089ab48ff99f4363c
LendoHub <lendodlic2022@gmail.com>
2022-09-13 15:01:23 -0700

    CHecks if value is positive or negative

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ *print_sign - prints '+' if number is greater than 0, '-' if number is negative and print 0 otherwise
+ *@n: the int to print
+ *
+ * Return: Always 0.
+ *
+ */
+
+int print_sign(int n)
+
+{
+
+	if (n > 0)
+	{
+
+		_putchar ('+');
+
+		return (1);
+
+	}
+
+
+
+	else if (n == 0)
+	{
+
+		_putchar ('0');
+
+		return (0);
+
+	}
+
+
+
+	else
+
+	{
+
+		_putchar ('-');
+
+		return (-1);
+
+	}
+
+
+
+}
