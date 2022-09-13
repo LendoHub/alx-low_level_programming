fc51f009f78745f38a357b9228b04a746b376dee
LendoHub <lendodlic2022@gmail.com>
2022-09-13 16:07:31 -0700

    Adding two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *
+ *add -prints added two numbers
+ *@i: print int i
+ *@k: print int k
+ *
+ *Return: Always 0.
+ */
+
+
+
+int add(int i, int k)
+{
+
+	return (i + k);
+
+}
