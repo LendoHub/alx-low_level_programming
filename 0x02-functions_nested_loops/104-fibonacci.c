87c772b5bc86828f529145a6ff3123a5cee15f79
LendoHub <lendodlic2022@gmail.com>
2022-09-13 21:25:11 -0700

    Third Fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,11 +5,9 @@
 *
 *Return: Always 0.
 */
-
 int main(void)
 {
 	int count;
-
 	unsigned long fib1 = 0, fib2 = 1, sum;
 	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
 	unsigned long half1, half2;
@@ -22,7 +20,6 @@ int main(void)
 		fib1 = fib2;
 		fib2 = sum;
 	}
-
 	fib1_half1 = fib1 / 10000000000;
 	fib2_half1 = fib2 / 10000000000;
 	fib1_half2 = fib1 % 10000000000;
@@ -38,20 +35,15 @@ int main(void)
 			half1 += 1;
 			half2 %= 10000000000;
 		}
-
 		printf("%lu%lu", half1, half2);
 
 		if (count != 98)
-			printf(", ");
-		
+		printf(", ");
 		fib1_half1 = fib2_half1;
 		fib1_half2 = fib2_half2;
 		fib2_half1 = half1;
 		fib2_half2 = half2;
-
 	}
-
 	printf("\n");
-
 	return (0);
 }
