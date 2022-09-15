aca92f6b82e86d4e52dcd2e03c1efc4b54b62f55
LendoHub <lendodlic2022@gmail.com>
2022-09-15 20:49:04 +0000

    Multiplying two integers

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 
 /**
- * _isdigit - function to check 0-9
- *
- * @c: checks input of function
- *
- * Return: returns 1 if `c` is digit
- * otherwise always 0 (Success)
- */
+*_isdigit - function to check 0-9
+*
+*@c: checks input of function
+*
+*Return: returns 1 if `c` is digit
+*otherwise always 0 (Success)
+*/
 
 int _isdigit(int c)
 {
