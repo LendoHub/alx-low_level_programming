afb04c750d59ce93f6e5ef95cab2a38583f2939e
LendoHub <lendodlic2022@gmail.com>
2022-09-20 14:40:43 +0000

    Prints array characters in reverse order

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*rev_string - a function that reverses a string
+*@s: input to reverse
+*Return: string and in reverse
+*/
+
+void rev_string(char *s)
+{
+	char reverse = s[0];
+	int fcount = 0;
+	int i;
+
+	while (s[fcount] != '\0')
+	fcount++;
+
+	for (i = 0; i < fcount; i++)
+	{
+		fcount--;
+		reverse = s[i];
+		s[i] = s[fcount];
+		s[fcount] = reverse;
+}
