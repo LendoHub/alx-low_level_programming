b933a33eb74717bae490e9ac70d1cfb28e518d5c
LendoHub <lendodlic2022@gmail.com>
2022-09-20 14:12:26 +0000

    Prints string in reverse order

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+*print_rev - Prints a string in reverse order
+*@s: String to reverse
+*Return: Nothing
+*/
+
+void print_rev(char *s)
+{
+	int length = strlen(s);
+
+	while (length--)
+	{
+		putchar(*(s + len));
+	}
+	putchar(10);
+}
