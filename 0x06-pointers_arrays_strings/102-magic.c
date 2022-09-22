07ecc4b19e673b6d1d712c6bc1a9faf02833fb0f
LendoHub <lendodlic2022@gmail.com>
2022-09-22 21:01:56 +0000

    Like Magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,28 +1,23 @@
-#include "main.h"
+#include <stdio.h>
 
-/**
-*rot13 - encodes a string using rot13.
-*@s: int type array pointer
-*Return: encoded
-*/
-
-char *rot13(char *s)
+int main(void)
 {
-	int i, ii;
-	char input[] =  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int n;
+	int a[5];
+	int *p;
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		for (ii = 0; ii < 54; ii++)
-		{
-			if (((s[i] <= 'z' && s[i] >= 'a') || (s[i] <= 'Z' && s[i] >= 'A'))
-				&& s[i] == input[ii])
-			{
-				s[i] = output[ii];
-				break;
-			}
-		}
-	}
-	return (s);
+	a[2] = 1024;
+	p = &n;
+	/*
+	*write your line of code here...
+	*Remember:
+	*- you are not allowed to use a
+	*- you are not allowed to modify p
+	*- only one statement
+	*- you are not allowed to code anything else than this line of code
+	*/
+	*(p + 5) = 98;
+	/* ...so that this prints 98\n */
+	printf("a[2] = %d\n", a[2]);
+	return (0);
 }
