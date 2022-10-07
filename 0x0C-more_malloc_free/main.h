6ffccc8485b5b44bd9e5f9b4c5b4ed161c82642f
LendoHub <lendodlic2022@gmail.com>
2022-10-07 04:50:10 +0000

    Multiplies two positive numbers

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 #include <limits.h>
 
+#define min(x, y) (((x) < (y)) ? (x) : (y))
+
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
