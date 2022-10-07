06981fc5a39e4470da25e1f05feb1e515e931ab7
LendoHub <lendodlic2022@gmail.com>
2022-10-07 04:25:43 +0000

    Multiplies two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 int find_len(char *str);
 char *create_xarray(int size);
@@ -232,5 +234,6 @@ int main(int argc, char *argv[])
 
 	free(next_prod);
 	free(final_prod);
+
 	return (0);
 }
