abbc204a4dd8384ab6670ee4b9a6fd67901ab08f
LendoHub <lendodlic2022@gmail.com>
2022-10-11 00:18:57 +0000

    Created a basic struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,24 @@
-#ifndef DOG
-#define DOG
+#ifndef STRCTEX
+#define STRCTEX
 
-#include <stdio.h>
-#include <stdlib.h>
+/**
+ * struct dog - creates a structure of a dog
+ * @name: Name of dog
+ * @age: age of dog
+ * @owner: the dog's owner
+ */
 
-#define struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+typedef struct dog dog1;
+
+void inti_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog1 *new_dog(char *name, float age, char *owner);
+void free_dog(dog1 *d);
 #endif /* DOG */
