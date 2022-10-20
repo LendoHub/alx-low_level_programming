b8cb7b8cf66e714c634d835e8fe65c8cfb93fb0e
LendoHub <lendodlic2022@gmail.com>
2022-10-20 19:43:50 +0000

    Adding new node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,40 +1,31 @@
 #include "lists.h"
 
 /**
-*_strlen - Returns the length of a string.
-*@s: The string whose the length should be returned.
-*Return: The length of s.
-*/
-
-int _strlen(const char *s)
-{
-	int i = -1;
-	char tmp;
-
-	do {
-		tmp = s[++i];
-	} while (tmp != '\0');
-	return (i);
-}
-
-/**
-*add_node - Adds a node to a single linked list
-*@head: A pointer to the pointer to the first element.
-*@str: The string to initialize with.
-*Return: The address of the item just added.
+*add_node - adds a new node at the beginning
+*of a list_t list.
+*@head: head of the linked list.
+*@str: string to store in the list.
+*Return: address of the head.
 */
 
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *l;
+	list_t *new_node;
+	size_t n;
 
-	l = malloc(sizeof(list_t));
-	if (l)
-	{
-		l->str = strdup(str);
-		l->len = _strlen(str);
-		l->next = *head;
-		*head = l;
-	}
-	return (1);
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+
+	for (n = 0; str[n]; n++)
+		;
+
+	new_node->len = n;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (*head);
+
 }
